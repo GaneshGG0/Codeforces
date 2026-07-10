ef1b9a51be2c1ea96bcbc209d01e0465daf3eb86
GaneshGG0 <ganeshshanbhag14@gmail.com>
2026-07-10 20:44:12 +0530

    Add Magnets [A] from Codeforces

--- 344/A - Magnets/solution.cpp
+++ 344/A - Magnets/solution.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
  
 int main() {
     int n;
     cin >> n;
  
-    int prev, cur;
+    string prev, cur;
     cin >> prev;
  
     int groups = 1;
@@ -13,8 +14,9 @@ int main() {
     for (int i = 1; i < n; i++) {
         cin >> cur;
  
-        if (cur != prev)
+        if (cur != prev) {
             groups++;
+        }
  
         prev = cur;
     }
